f7f7256c352357492af4f246d0f3f6d6dfa076c7
Aditya Kakarla <85147239+adityakakarla@users.noreply.github.com>
2025-01-27 22:42:53 -0800

    updated

--- app/actions.ts
+++ app/actions.ts
@@ -14,96 +14,96 @@ import OpenAI from 'openai';
 
 export async function generateResponse(prompt: string, temperature: number) {
     try {
-        const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY});
+    //     const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY});
 
-        const flagged = await openai.moderations.create({
-            input: prompt
-        })
+    //     const flagged = await openai.moderations.create({
+    //         input: prompt
+    //     })
 
-        if(flagged.results[0]?.flagged) {
-            throw new Error(`Your prompt has been flagged. Please try again.`)
-        }
+    //     if(flagged.results[0]?.flagged) {
+    //         throw new Error(`Your prompt has been flagged. Please try again.`)
+    //     }
 
 
-        const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY
-        if (!supabaseKey) throw new Error(`Expected SUPABASE_SERVICE_ROLE_KEY`)
+    //     const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY
+    //     if (!supabaseKey) throw new Error(`Expected SUPABASE_SERVICE_ROLE_KEY`)
 
-        const url = process.env.SUPABASE_URL
-        if (!url) throw new Error(`Expected env var SUPABASE_URL`)
+    //     const url = process.env.SUPABASE_URL
+    //     if (!url) throw new Error(`Expected env var SUPABASE_URL`)
 
-        const supabase = createClient(url as string, supabaseKey as string)
+    //     const supabase = createClient(url as string, supabaseKey as string)
 
-        const retriever = new SupabaseVectorStore(
-            new OpenAIEmbeddings({ model: 'text-embedding-3-small' }),
-            {
-                client: supabase,
-                tableName: 'documents',
-                queryName: 'match_documents',
-            }
-        ).asRetriever(5)
+    //     const retriever = new SupabaseVectorStore(
+    //         new OpenAIEmbeddings({ model: 'text-embedding-3-small' }),
+    //         {
+    //             client: supabase,
+    //             tableName: 'documents',
+    //             queryName: 'match_documents',
+    //         }
+    //     ).asRetriever(5)
 
-        const openAIApiKey = process.env.OPENAI_API_KEY;
+    //     const openAIApiKey = process.env.OPENAI_API_KEY;
 
-        const llm = new ChatOpenAI({ openAIApiKey, temperature, model: 'gpt-4o-mini' });
+    //     const llm = new ChatOpenAI({ openAIApiKey, temperature, model: 'gpt-4o-mini' });
 
-        const standaloneQuestionTemplate = `Given a question, convert it into a standalone question.
+    //     const standaloneQuestionTemplate = `Given a question, convert it into a standalone question.
 
-    question: {question}
-    standalone question:`;
+    // question: {question}
+    // standalone question:`;
 
-        const standaloneQuestionPrompt = PromptTemplate.fromTemplate(
-            standaloneQuestionTemplate
-        );
+    //     const standaloneQuestionPrompt = PromptTemplate.fromTemplate(
+    //         standaloneQuestionTemplate
+    //     );
 
-        const standaloneQuestionChain = RunnableSequence.from([
-            standaloneQuestionPrompt,
-            llm,
-            new StringOutputParser(),
-        ]);
+    //     const standaloneQuestionChain = RunnableSequence.from([
+    //         standaloneQuestionPrompt,
+    //         llm,
+    //         new StringOutputParser(),
+    //     ]);
 
-        const retrieverChain = RunnableSequence.from([
-            (prevResult) => prevResult.standalone_question,
-            retriever,
-            combineDocuments,
-            new StringOutputParser(),
-        ]);
+    //     const retrieverChain = RunnableSequence.from([
+    //         (prevResult) => prevResult.standalone_question,
+    //         retriever,
+    //         combineDocuments,
+    //         new StringOutputParser(),
+    //     ]);
 
-        const answerTemplate = `
-    Answer the potentially UCSD-related question. Use context if needed.
+    //     const answerTemplate = `
+    // Answer the potentially UCSD-related question. Use context if needed.
     
-    Speak in a friendly tone. Answer in 1-2 sentences. Be concise and succinct.
+    // Speak in a friendly tone. Answer in 1-2 sentences. Be concise and succinct.
 
-    If you are not sure of the answer, say so.
+    // If you are not sure of the answer, say so.
     
-    context: {context}
-    question: {question}
-    answer: `;
+    // context: {context}
+    // question: {question}
+    // answer: `;
 
-        const answerPrompt = PromptTemplate.fromTemplate(answerTemplate);
+    //     const answerPrompt = PromptTemplate.fromTemplate(answerTemplate);
 
-        const answerChain = RunnableSequence.from([
-            answerPrompt,
-            llm,
-            new StringOutputParser(),
-        ]);
+    //     const answerChain = RunnableSequence.from([
+    //         answerPrompt,
+    //         llm,
+    //         new StringOutputParser(),
+    //     ]);
 
-        const chain = RunnableSequence.from([
-            {
-                standalone_question: standaloneQuestionChain,
-                original_input: new RunnablePassthrough(),
-            },
-            {
-                context: retrieverChain,
-                question: ({ original_input }) => original_input.question,
-            },
-            answerChain,
-        ]);
+    //     const chain = RunnableSequence.from([
+    //         {
+    //             standalone_question: standaloneQuestionChain,
+    //             original_input: new RunnablePassthrough(),
+    //         },
+    //         {
+    //             context: retrieverChain,
+    //             question: ({ original_input }) => original_input.question,
+    //         },
+    //         answerChain,
+    //     ]);
 
-        const response = await chain.stream({
-            question: prompt,
-        });
+    //     const response = await chain.stream({
+    //         question: prompt,
+    //     });
 
-        const stream = createStreamableValue(response);
+        const stream = createStreamableValue('Shut this down because Supabase has a low free tier :(');
         return stream.value;
     } catch (error) {
         const stream = createStreamableValue(`${error}`)
